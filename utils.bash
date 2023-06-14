803e97444335bd402dd4e73db4ffe11e76515a1a
dmgie <danmgie@outlook.com>
2023-06-14 14:57:43 +0200

    Modify the display_menu function to accept a comma
    delimited list

--- utils.bash
+++ utils.bash
@@ -1,38 +1,13 @@
 #!/usr/bin/env bash
 
 ## This is a file that contains shared functions throughout the scripts
-#
-# display_menu() {
-#     # Create a display menu for the user to select from, and return the selected option (echo'd)
-#     local prompt=$1
-#     shift
-#     local options=("$@")
-#     local num_options=${#options[@]}
-#
-#     echo "$prompt" >&2
-#     for ((i=0; i<num_options; i++)); do
-#         echo "$((i+1)). ${options[i]}" >&2
-#     done
-#
-#     exec < /dev/tty
-#
-#     read -rp $'Enter your choice [1-'$num_options']: ' choice
-#
-#     if [[ $choice =~ ^[1-$num_options]$ ]]; then
-#         selected_option=${options[choice-1]}
-#         echo "$selected_option"
-#     else
-#         echo "Invalid choice. Please try again." >&2
-#         display_menu "$prompt" "${options[@]}"
-#     fi
-# }
-#
-
-
 display_menu() {
     local prompt=$1
-    shift
-    local options=("$@")
+    local options_string=$2
+
+    # Set the Internal Field Separator (IFS) to comma
+    IFS=',' read -ra options <<< "$options_string"
+
     local num_options=${#options[@]}
 
     echo "$prompt" >&2
@@ -46,7 +21,7 @@ display_menu() {
     local choice
 
     while true; do
-        read -rp $'Enter your choice [1-'$num_options'] (Enter to finish): ' choice
+        read -rp $'[1mEnter your choice [1-'$num_options'] (Enter to finish)[0m: ' choice
         if [[ -z $choice ]]; then
             break
         elif [[ $choice =~ ^[1-$num_options]$ ]]; then
