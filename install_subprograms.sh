9f53b8e184dce478f4a17974b3394eb1ea80f2b8
dmgie <danmgie@outlook.com>
2023-06-20 18:23:26 +0200

    Add gitignore for programs, and add more programs to the script

--- install_subprograms.sh
+++ install_subprograms.sh
@@ -3,7 +3,7 @@
 
 # minimap2
 # check if in programs/minimap2 or if minimap2 is in the path
-if [ ! -d ./programs/minimap2 ] || command -v minimap2 >/dev/null 2>&1; then
+if [ ! -x ./programs/minimap2/minimap2 ] || command -v minimap2 >/dev/null 2>&1; then
     git clone --depth=1 www.github.com/lh3/minimap2 programs/minimap2
     cd programs/minimap2
     make
@@ -28,5 +28,32 @@ if [ ! -d ./programs/samtools-1.17 ] || command -v samtools >/dev/null 2>&1; the
     make install
     cd ../..
 fi
+
+# bcftools
+# This is needed for samtools
+if [ ! -d ./programs/bcftools-1.17 ] || command -v bcftools >/dev/null 2>&1; then
+    wget https://github.com/samtools/bcftools/releases/download/1.17/bcftools-1.17.tar.bz2 -O programs/bcftools.tar.bz2
+    tar -xjf programs/bcftools.tar.bz2 -C programs/
+    rm programs/bcftools.tar.bz2
+    cd programs/bcftools-1.17
+    ./configure --prefix=../ # To install into programs folder
+    make
+    make install
+    cd ../..
+fi
+
+# htslib
+# This is needed for bcftools/samtools
+if [ ! -d ./programs/htslib-1.17 ] || command -v htsfile >/dev/null 2>&1; then
+    wget https://github.com/samtools/htslib/releases/download/1.17/htslib-1.17.tar.bz2 -O programs/htslib.tar.bz2
+    tar -xjf programs/htslib.tar.bz2 -C programs/
+    rm programs/htslib.tar.bz2
+    cd programs/htslib-1.17
+    ./configure --prefix=../ # To install into programs folder
+    make
+    make install
+    cd ../..
+fi
+
 
 
