3ea79678cb16ad94cb394ceb0e599e0416cf8c18
dmgie <danmgie@outlook.com>
2023-07-06 11:22:41 +0200

    Only run GATK pipeline for selected assembly
    methods

--- scripts/GATK_subscript.bash
+++ scripts/GATK_subscript.bash
@@ -3,60 +3,57 @@
 
 # Resource: https://gatk.broadinstitute.org/hc/en-us/articles/360035531192-RNAseq-short-variant-discovery-SNPs-Indels-
 
-GENOME=$1 # The human genome assembly
-BAM=$2 # The input bam file
+GENOME=$1 # The human genome reference
 OUTPUT_DIR=$3 # The output directory
 
 # Source arrays from temp_variables.sh
 source $WORKING_DIR/temp_variables.sh
 
+# Read the value of rna_categories["Assembly"] and turn the string into a list by splitting the string by the delimiter ";"
+# Then, loop through the list and run the GATK pipeline for each assembly
+IFS='§' read -ra ASSEMBLIES <<< "${rna_categories["Assembly"]}"
+for assembly_program in "${ASSEMBLIES[@]}"; do
+    # check if the assembly program value is 1 in rna_programs assosciative array
+    if [ ${rna_programs[$assembly_program]} -eq 1 ]; then
+        # Run the GATK pipeline
+        echo "Running GATK pipeline for ${assembly_program}"
 
-# TODO: Split the string then loop over it
-assembly_programs=$(${rna_categories["Assembly"]} | tr '§' ' ')
-for program in $assembly_programs; do
-    if [[ rna_programs["${program}"] -eq 1 ]]; then
-        echo $program
+        # Preprocessing required to make formatting for RNA-aligned output similar to DNA aligned output
+        # which is needed for HaploType caller later
+        echo "Adjusting RNA bam output to allow for Haplotype caller"
+        gatk SplitNCigarReads \
+            -R $GENOME \
+            -I $BAM \
+            -O ${OUTPUT_DIR}/${BAM}-split.bam
+
+        # OPTIONAL: If known polymorphic sites (.vcf file) are there, we can skip over them to exclude from recalibration
+        # If no known sites, use "--unsorted" (use all sites for recalibration) or "--interval" (specific interval of fasta file used for calibration)
+        echo "Recalibrating bases"
+
+        # Actual Haplotype calling
+        echo "Running Haplotype caller to analyse variants"
+        gatk --java-options "-Xmx4g" HaplotypeCaller  \
+            -R $GENOME \
+            -I $BAM \
+            -O $BAM-output.g.vcf.gz \
+            -ERC GVCF
+            # Alternatively
+            gatk --java-options "-Xmx4g" HaplotypeCaller  \
+                -R $GENOME \
+                -I $BAM \
+                -O ${OUTPUT_DIR}/${BAM}-output.g.vcf.gz \
+                -bamout $BAM-bamout.bam
+
+        # Variant Filtering
+        # As there is not much truth data for training for the CNN/VQSR methods, use hard filtering
+        echo "Running variant filtration for..."
+        gatk VariantFiltration \
+            -R $GENOME \
+            -V ${OUTPUT_DIR}/${BAM}-output.g.vcf.gz \
+            -O output.vcf.gz \
+            --filter-name "my_filter1" \
+            --filter-expression "AB < 0.2" \
+            --filter-name "my_filter2" \
+            --filter-expression "MQ0 > 50"
     fi
 done
-
-# Preprocessing required to make formatting for RNA-aligned output similar to DNA aligned output
-# which is needed for HaploType caller later
-echo "Adjusting RNA bam output to allow for Haplotype caller"
-gatk SplitNCigarReads \
-    -R $GENOME \
-    -I $BAM \
-    -O ${OUTPUT_DIR}/${BAM}-split.bam
-
-
-# OPTIONAL: If known polymorphic sites (.vcf file) are there, we can skip over them to exclude from recalibration
-# If no known sites, use "--unsorted" (use all sites for recalibration) or "--interval" (specific interval of fasta file used for calibration)
-echo "Recalibrating bases"
-
-
-
-# Actual Haplotype calling
-echo "Running Haplotype caller to analyse variants"
-gatk --java-options "-Xmx4g" HaplotypeCaller  \
-    -R $GENOME \
-    -I $BAM \
-    -O $BAM-output.g.vcf.gz \
-    -ERC GVCF
-# Alternatively
-gatk --java-options "-Xmx4g" HaplotypeCaller  \
-    -R $GENOME \
-    -I $BAM \
-    -O ${OUTPUT_DIR}/${BAM}-output.g.vcf.gz \
-    -bamout $BAM-bamout.bam
-
-
-# Variant Filtering
-# As there is not much truth data for training for the CNN/VQSR methods, use hard filtering
-echo "Running variant filtration for..."
-gatk VariantFiltration \
-    -R $GENOME \
-    -V ${OUTPUT_DIR}/${BAM}-output.g.vcf.gz \
-    -O output.vcf.gz \
-    --filter-name "my_filter1" \
-    --filter-expression "AB < 0.2" \
-    --filter-name "my_filter2" \
-    --filter-expression "MQ0 > 50"
