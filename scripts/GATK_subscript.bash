773826f9b2674548c5f5050132a7147f12da4eb6
Sebastian Meyer <s.meyer@student.uni-tuebingen.de>
2023-07-06 11:42:18 +0200

    Added Trinity check input functionality

--- scripts/GATK_subscript.bash
+++ scripts/GATK_subscript.bash
@@ -1,44 +1,41 @@
 #!/usr/bin/env bash
 
-
 # Resource: https://gatk.broadinstitute.org/hc/en-us/articles/360035531192-RNAseq-short-variant-discovery-SNPs-Indels-
 
 GENOME=$1 # The human genome assembly
-BAM=$2 # The input bam file
 OUTPUT_DIR=$3 # The output directory
 
 # Source arrays from temp_variables.sh
 source $WORKING_DIR/temp_variables.sh
 
-# Preprocessing required to make formatting for RNA-aligned output similar to DNA aligned output
-# which is needed for HaploType caller later
+# The input bam file
+BAM="${OUTPUT_DIR}_${program}_output/" *.bam
+
+#Preprocessing required to make formatting for RNA-aligned output similar to DNA aligned output
+#which is needed for HaploType caller later
 echo "Adjusting RNA bam output to allow for Haplotype caller"
 gatk SplitNCigarReads \
     -R $GENOME \
     -I $BAM \
     -O ${OUTPUT_DIR}/${BAM}-split.bam
 
-
 # OPTIONAL: If known polymorphic sites (.vcf file) are there, we can skip over them to exclude from recalibration
 # If no known sites, use "--unsorted" (use all sites for recalibration) or "--interval" (specific interval of fasta file used for calibration)
 echo "Recalibrating bases"
 
-
-
 # Actual Haplotype calling
 echo "Running Haplotype caller to analyse variants"
-gatk --java-options "-Xmx4g" HaplotypeCaller  \
+gatk --java-options "-Xmx4g" HaplotypeCaller \
     -R $GENOME \
     -I $BAM \
     -O $BAM-output.g.vcf.gz \
     -ERC GVCF
 # Alternatively
-gatk --java-options "-Xmx4g" HaplotypeCaller  \
+gatk --java-options "-Xmx4g" HaplotypeCaller \
     -R $GENOME \
     -I $BAM \
     -O ${OUTPUT_DIR}/${BAM}-output.g.vcf.gz \
     -bamout $BAM-bamout.bam
-
 
 # Variant Filtering
 # As there is not much truth data for training for the CNN/VQSR methods, use hard filtering
--- scripts/Trinity_subscript.bash
+++ scripts/Trinity_subscript.bash
@@ -1,43 +1,58 @@
 #!/usr/bin/env bash
 
 MODE="$1"
-input_dir="$2"
 human_reference="$3"
 output_dir="$4"
 num_cores="$5"
 max_ram="$6"
 max_intron="$7"
 
-## Trinity DeNovo / Genome Guided Assembly
+source $WORKING_DIR/temp_variables.sh
 
-case $MODE in
+IFS="§"
 
-DeNovo)
-  echo "Trinity DeNovo Assembly Mode"
-  for file in "${input_dir}/"*fastq; do
+for program in ${rna_categories["Quality Control"]}; do
+  if [ "${rna_programs[$program]}" -eq 1 ]; then
+    echo "Aktueller Wert #####: $program"
 
-    Trinity --seqType fq \
-      --max_memory $max_ram \
-      --CPU $num_cores \
-      --single $file \
-      --output ${output_dir}/trinity_output
+    ## Trinity DeNovo / Genome Guided Assembly
 
-    echo "The file $file has been processed."
-  done
-  ;;
+    input_dir="${output_dir}${program}_output/"
 
-Ref_Based)
-  echo "Trinity Genome-Guided Assembly Mode"
-  for file in "${input_dir}/"*fastq; do
-   Trinity --genome_guided_bam $input_dir \
-           --genome_guided_max_intron $max_intron \
-           --max_memory $max_ram  \
-           --CPU $num_cores 
-  done
-  ;;
+    echo $input_dir
 
-*)
-  echo "Invalid MODE: $MODE use DeNovo or Ref_Based instead."
-  exit 1
+    case $MODE in
 
-esac
+    DeNovo)
+      echo "Trinity DeNovo Assembly Mode"
+      for file in "${input_dir}/"*fastq; do
+
+        Trinity --seqType fq \
+          --max_memory $max_ram \
+          --CPU $num_cores \
+          --single $file \
+          --output ${output_dir}/trinity_output
+
+        echo "The file $file has been processed."
+      done
+      ;;
+
+    Ref_Based)
+      echo "Trinity Genome-Guided Assembly Mode"
+      for file in "${input_dir}/"*fastq; do
+        Trinity --genome_guided_bam $input_dir \
+          --genome_guided_max_intron $max_intron \
+          --max_memory $max_ram \
+          --CPU $num_cores
+      done
+      ;;
+
+    *)
+      echo "Invalid MODE: $MODE use DeNovo or Ref_Based instead."
+      exit 1
+      ;;
+
+    esac
+
+  fi
+done
