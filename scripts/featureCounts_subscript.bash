d8309bdd64e5af2c1c3aad77c7969aa0a7bd263c
Sebastian Meyer <s.meyer@student.uni-tuebingen.de>
2023-07-13 11:39:27 +0200

    Adapted featureCounts_subscript.bash to take multiple input files

--- scripts/featureCounts_subscript.bash
+++ scripts/featureCounts_subscript.bash
@@ -2,8 +2,26 @@
 
 # This is one way to get a quantification of transcripts/reads using a given BAM, GFF file
 
-BAM=$1
+BAM_DIR=$1
 GFF=$2
-FEATURE=$3 # What feature to use in the GTF/GFF file i.e exon, gene, transcript
+FEATURE=$3     # What feature to use in the GTF/GFF file i.e exon, gene, transcript
+ATTRIBUTE=$4   # In Prokaryotic dataset it is ID
+OUTPUT_DIR=$5  
 
-featureCounts -F GTF -t $FEATURE -a $GFF -o ${BAM}_counts.txt $BAM
+mkdir -p "${OUTPUT_DIR}/featureCounts_output"
+
+for bam_file in "$BAM_DIR"/*.bam; do
+  if [ -f "$bam_file" ]; then
+
+    output_file="${bam_file##*/}"
+    output_file="${output_file%.bam}_counts.txt"
+
+    featureCounts -F GTF        \
+                  -t $FEATURE   \
+                  -g $ATTRIBUTE \
+                  -T 4          \
+                  -a "$GFF"     \
+                  -o "$OUTPUT_DIR/featureCounts_output/$output_file" \
+                  "$bam_file"
+  fi
+done
