fe4dc32895647529855adac54044123b8b26fcdc
dmgie <danmgie@outlook.com>
2023-07-21 13:02:18 +0200

    Remove all of the previous workflow-related files
    This is a restart from the beginning, but kept in commit history for
    reference later on

--- scripts/coverage_script.bash
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-# https://www.biostars.org/p/49306/ -- use the -M option for much faster
-# Create a BED file using the existing chromosomes
--- scripts/salmon_subscript.bash
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-
-salmon quant -i transcripts_index -l <LIBTYPE> -r reads.fq --validateMappings -o transcripts_quant
