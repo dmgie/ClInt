3957ca74d29c91cfe759d105c2f1e0562231f224
Sebastian Meyer <s.meyer@student.uni-tuebingen.de>
2023-07-24 12:59:44 +0200

    added filtered .gff file creation

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,4 +1,5 @@
 import os
+import pysam
 
 def extract_attribute(input_string, query):
     start = input_string.find(query)
@@ -11,6 +12,17 @@ def extract_attribute(input_string, query):
         return None
 
     return input_string[start:end]
+
+def create_gff(gff_file_new, header_lines, extracted_lines):
+    with open(gff_file_new, 'w') as f:
+        # Schreibe die Header-Zeilen in die Datei
+        for header_line in header_lines:
+            f.write(f"{header_line[0]}\n")
+        
+        # Schreibe die Inhaltszeilen in die Datei
+        for line in extracted_lines:
+            gff_line = "\t".join(line)
+            f.write(f"{gff_line}\n")
 
 def read_vcf_file(types):
     variants_dict = {}
@@ -31,15 +43,7 @@ def read_vcf_file(types):
                         info = record.info
                         sample_info = record.samples
 
-                        # print(f"Chromosom: {chrom}")
-                        # print(f"Position: {pos}")
-                        # print(f"Referenz: {ref}")
-                        # print(f"Alternativen: {alt}")
-                        # print(f"Qualität: {qual}")
-                        # print(f"Filter-Status: {filter_status}")
-                        # print(f"Info: {info}")
-                        # print(f"Sample-Informationen: {sample_info}")
-                        # print("-" * 30)
+                        # print_variants(chrom, pos, ref, alt, qual, filter_status, info, sample_info)
                         
                         if pos in variants_dict:
                             if type in variants_dict[pos]:
@@ -56,3 +60,43 @@ def read_vcf_file(types):
                               "chromosome":chrom
                             }         
     return variants_dict
+
+def print_variants():
+    print(f"Chromosom: {chrom}")
+    print(f"Position: {pos}")
+    print(f"Referenz: {ref}")
+    print(f"Alternativen: {alt}")
+    print(f"Qualität: {qual}")
+    print(f"Filter-Status: {filter_status}")
+    print(f"Info: {info}")
+    print(f"Sample-Informationen: {sample_info}")
+    print("-" * 30)
+
+
+def get_gff_header(gff_file):
+    header_lines=[]
+    with open(gff_file, 'r+') as infile:
+        for line in infile:
+            if line.startswith('#'):
+               header_lines.append(line.strip().split("\t"))
+    return header_lines
+
+def get_gff_lines(gff_file, chromosome, position, extend, feature, extracted_lines):
+    with open(gff_file, 'r+') as infile:
+        for line in infile:
+            if not line.startswith('#'):
+                fields = line.strip().split('\t')
+                if len(fields) >= 5: 
+                    chr_name, source, feature_type, start, end, *_ = fields
+                    if feature_type == feature:
+                        if chr_name == str(chromosome): 
+                            start_pos, end_pos = int(start), int(end)
+                            if (start_pos - extend) <= position <= (end_pos + extend):
+                                extracted_lines.append(line.strip())
+                                
+        if not extracted_lines:
+            extend += 10
+            return get_gff_lines(gff_file, chromosome, position, extend, feature, extracted_lines)
+            
+    print(f"Result found at extension {extend}")                                                
+    return extracted_lines
--- scripts/vartable.py
+++ scripts/vartable.py
@@ -1,76 +1,5 @@
-import pysam
-
-import utils
-
-
-def print_results(variants, gff_file, feature):
-    matches_count = 0
-    total_count = 0
-    
-    print("Variants appearing at the same position in DNA and RNA\n")
-    for idx, variant in enumerate(variants, start=1):
-           
-        if "rna" in variants[variant] and "dna" in variants[variant]:
-            print(f"\nVariant {idx}")
-            print(variant, "REF:", variants[variant]["ref"], "ALT DNA:", variants[variant]["dna"], "ALT RNA:", variants[variant]["rna"])
-            extracted_lines=get_gff_lines(gff_file, variants[variant]["chromosome"], variant, 0, feature, [])
-            for line in extracted_lines:
-                values = line.split('\t')
-                print(f"Found Variant at gene: {extract_attribute(values[8], 'Name=')}, ID:{extract_attribute(values[8], 'ID=')}, Start:{values[3]}, End:{values[4]}")
-            
-            print("Variants appearing in files:")
-            for hit in variants[variant]["hits"]:
-                print(hit)
-            matches_count += 1
-
-            
-            
-        total_count += 1
-        
-    print(f"\n\nCounted {matches_count} dna/rna variant matches, {total_count} variants in total ({round(matches_count/total_count,2)}%).")
-    
-
-def get_gff_lines(gff_file, chromosome, position, extend, feature, extracted_lines):
-    
-    #header_lines=[]
-    with open(gff_file, 'r+') as infile:
-        for line in infile:
-            if not line.startswith('#'):
-                fields = line.strip().split('\t')
-                if len(fields) >= 5: 
-                    chr_name, source, feature_type, start, end, *_ = fields
-                    if feature_type == feature:
-                        if chr_name == str(chromosome): 
-                            start_pos, end_pos = int(start), int(end)
-                            if (start_pos - extend) <= position <= (end_pos + extend):
-                                extracted_lines.append(line.strip())
-            #else:
-             #   header_lines.append(line.strip())
-                                
-        if not extracted_lines:
-            extend += 10
-            return get_gff_lines(gff_file, chromosome, position, extend, feature, extracted_lines)
-            
-            
-    # try:
-    #     mode = 'a' if os.path.exists("./gff_new.gff") else 'w'
-    #     with open("gff_new.gff", mode) as outfile:
-    #         for line in header_lines:
-    #             outfile.write(f"{line}\n")
-    #         for line in extracted_lines:
-    #             outfile.write(f"{line}\n")
-    # except Exception as e:
-    #     print("Error at writing .gff file")   
-        
-        
-    print(f"Result found at extension {extend}")                                                
-    return extracted_lines
-    
-
-
-    
-    
-           
+import os
+from utils import *
 
 if __name__ == "__main__":
 
@@ -82,15 +11,31 @@ if __name__ == "__main__":
     gff_file = "../../../../local_scratch/ClINT/RawData-RNA/ref_genome.gff"
     gff_file_new = "./gff_new.gff"
     feature = "gene"
-    
-    header_lines=[]
-    
-    # mode = 'a' if os.path.exists(gff_file_new) else 'w'
-    # with open(gff_file_new, mode) as infile: 
-    #     for line in infile:   
-    #         header_lines.append(line.strip() + "\n")
-    
 
+    header_lines=[]
+    gff_lines=[]
     variants = read_vcf_file(dir_dict)
-    print_results(variants, gff_file, feature)
+    matches_count = 0
+    total_count = 0
+    
+    print("Variants appearing at the same position in DNA and RNA\n")
+    for idx, variant in enumerate(variants, start=1):
+           
+        if "rna" in variants[variant] and "dna" in variants[variant]:
+            print(f"\nVariant {idx}")
+            print(variant, "REF:", variants[variant]["ref"], "ALT DNA:", variants[variant]["dna"], "ALT RNA:", variants[variant]["rna"])
+            extracted_lines=get_gff_lines(gff_file, variants[variant]["chromosome"], variant, 0, feature, [])
+            gff_lines.append(extracted_lines)
+            for line in extracted_lines:
+                values = line.split('\t')
+                print(f"Found Variant at gene: {extract_attribute(values[8], 'Name=')}, ID:{extract_attribute(values[8], 'ID=')}, Start:{values[3]}, End:{values[4]}")
+            
+            print("Variants appearing in files:")
+            for hit in variants[variant]["hits"]:
+                print(hit)
+            matches_count += 1     
+        total_count += 1
+        
+    print(f"\n\nCounted {matches_count} dna/rna variant matches, {total_count} variants in total ({round(matches_count/total_count,2)}%).")
+    create_gff(gff_file_new, get_gff_header(gff_file), gff_lines)
     
