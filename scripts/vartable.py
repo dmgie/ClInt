500ca520cfbdbfef3ebd479e609eff64fcf1a392
Sebastian Meyer <s.meyer@student.uni-tuebingen.de>
2023-07-27 13:56:11 +0200

    integrated counts to output file

--- scripts/vartable.py
+++ scripts/vartable.py
@@ -1,5 +1,6 @@
 import os
 import csv
+import numpy as np
 from utils import *
 
 if __name__ == "__main__":
@@ -24,32 +25,29 @@ if __name__ == "__main__":
     matches=[]
     matches_count = 0
     total_count = 0
+
+    output_line=[]
     
     with open(f'./{output_dir}/output.tsv', 'wt') as out_file:
         tsv_writer = csv.writer(out_file, delimiter='\t')
         tsv_writer.writerow(['Gene_ID', "Gene_Name", 'Start', 'End', 'Variant_Position', 'REF', "DNA_ALT", "RNA_ALT", "Counts"])
         print("Variants appearing at the same position in DNA and RNA\n")
         for idx, variant in enumerate(variants, start=1):
-           
             if "rna" in variants[variant] and "dna" in variants[variant]:
                 print(f"\nVariant {idx}")
                 print(variant, "REF:", variants[variant]["ref"], "ALT DNA:", variants[variant]["dna"], "ALT RNA:", variants[variant]["rna"])
-                extracted_lines=get_gff_lines(gff_file, variants[variant]["chromosome"], variant, 0, feature, [])
+                extracted_lines = get_gff_lines(gff_file, variants[variant]["chromosome"], variant, 0, feature, [])
                 gff_lines.append(extracted_lines)
                 for line in extracted_lines:
                     values = line.split('\t')
-                    gene_name=extract_attribute(values[8], 'Name=')
-                    gene_id=extract_attribute(values[8], 'ID=')
+                    gene_name = extract_attribute(values[8], 'Name=')
+                    gene_id = extract_attribute(values[8], 'ID=')
                     start=values[3]
                     end=values[4]
 
-                    tsv_writer.writerow([gene_id, gene_name, start, end, variant, variants[variant]["ref"], variants[variant]["dna"], variants[variant]["rna"]])
-
-                    print(f"Found Variant at gene: {gene_name}, ID:{gene_id}, Start:{start}, End:{end}")
-            
-                print("Variants appearing in files:")
+                    output_line.append({"out":[gene_id, gene_name, start, end, variant, variants[variant]["ref"], variants[variant]["dna"], variants[variant]["rna"]], "files":variants[variant]["hits"]})
+                   
                 for hit in variants[variant]["hits"]:
-                    print(hit)
                     if hit not in matches:
                         matches.append(hit) 
                 
@@ -58,31 +56,12 @@ if __name__ == "__main__":
         
         print(f"\n\nCounted {matches_count} dna/rna variant matches, {total_count} variants in total ({round(matches_count/total_count,2)}%).")
 
-    create_gff(gff_file_new, get_gff_header(gff_file), gff_lines)
+        create_gff(gff_file_new, get_gff_header(gff_file), gff_lines)
+        #run_featurecounts(bam_file_string, gff_file_new, feature_counts_output, "gene")
 
-
-    bam_file_string = " ".join(str(dir_dict["bam"]+match) for match in matches)
-    bam_file_string = remove_prefix_and_suffix(bam_file_string)
-
-    #run_featurecounts(bam_file_string, gff_file_new, feature_counts_output, "gene")
-    count = get_expression_count(feature_counts_output, "ACCFDFCE_00647", "../../../../local_scratch/ClINT/working_files/deduped_bams/dedup_snc_trimmed_Q12SA022AP_20211014141226__2109SHr1022_10_S22_R1_001.bam")
-
-    
-
-    
-
-
-
-
-
-
-    # for variant in variants:
-    #     if "rna" in variants[variant] and "dna" in variants[variant]:
-    #         extracted_lines=get_gff_lines(gff_file, variants[variant]["chromosome"], variant, 0, feature, [])
-    #         for line in extracted_lines:
-    #             print("#########Count: ", line)
-    #             for hit in variants[variant]["hits"]:
-    #                 print(hit, "Expression Counts:", get_expression_count(feature_counts_output, {extract_attribute(values[8], 'ID=').replace("_gene", "")}, dir_dict["bam"]+remove_prefix_and_suffix(hit)))
-    #                 if hit not in matches:
-    #                     matches.append(hit) 
-    
+        for row in output_line:
+            counts = ""
+            for file in row["files"]:
+                counts += str(get_expression_count(feature_counts_output, row["out"][0].replace("_gene", ""), dir_dict["bam"] + remove_prefix_and_suffix(file)))+","  
+            out_line=row["out"]+[counts]
+            out_file.write('\t'.join(map(str, out_line)) + '\n')
