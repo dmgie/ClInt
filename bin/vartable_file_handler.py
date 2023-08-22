318e443f8f39ee535d5f3b4cd391ba62a771dc02
Sebastian Meyer <s.meyer@student.uni-tuebingen.de>
2023-08-22 11:22:23 +0200

    implemented metafile parsing

--- bin/vartable_file_handler.py
+++ bin/vartable_file_handler.py
@@ -7,8 +7,7 @@ import glob
 #### -> Takes the top level VCF directory as input, runs vartable.py on all folders below
 #### -> Define vartable input arguments in this file
 
-if __name__ == "__main__":
-
+def main():
     ## Argument parsing
 
     parser = argparse.ArgumentParser(description="File handler for VarTable Analysis")
@@ -16,7 +15,61 @@ if __name__ == "__main__":
     args = parser.parse_args()
     vcf_path = getattr(args, "vcf_path")
 
-    ## Get all directories below vcf_path
+    # metafile_path = "./Q001H_sample_preparations_20230803115337.tsv"
+    # meta_file_sorted = get_meta_file(metafile_path)
+
+    # for patient in meta_file_sorted:
+    #     print("Patient:", patient)
+    #     for sample in meta_file_sorted[patient]:
+    #         print(sample, meta_file_sorted[patient][sample])
+    #     print("\n")
+
+    # print("Number of patients:", len(meta_file_sorted))
+
+    execute_vartable(vcf_path)
+
+
+def natural_sort_key(s):
+        def atoi(text):
+            return int(text) if text.isdigit() else text
+        return [atoi(c) for c in s.split()]
+
+def sort_dict_by_keys(dictionary):
+    sorted_items = sorted(dictionary.items(), key=lambda x: natural_sort_key(x[0]))
+    sorted_dict = dict(sorted_items)
+    return sorted_dict
+
+def get_meta_file(metafile_path):
+    meta_file = {}
+
+    with open(metafile_path, "r") as metafile:
+        lines = metafile.readlines()
+
+        for line in lines[1:]:
+            line_content = line.strip().split('\t')
+
+            patient_id = line_content[4]
+            qbic_identifier = line_content[0]
+            alt_identifier = line_content[1]
+
+            if "BCR" not in alt_identifier and "TCR"  not in alt_identifier:
+                if patient_id in meta_file:
+                    ## Patient # already in dict, extend
+                    sample_number = f"Sample_{len(meta_file[patient_id])+1}"
+                    dict_entry = {sample_number:{'qbic_identifier': qbic_identifier,'alt_identifier': alt_identifier}}
+                    meta_file[patient_id].update(dict_entry)
+
+                else:
+                    ## New dict entry
+                    meta_file[patient_id] = {"Sample_1":{
+                        'qbic_identifier': qbic_identifier,
+                        'alt_identifier': alt_identifier
+                    }}
+            
+    return sort_dict_by_keys(meta_file)
+
+def execute_vartable(vcf_path):
+    # Get all directories below vcf_path
     directories = glob.glob(f'{vcf_path}/*/')
 
     for d in directories: print(d)
@@ -40,3 +93,5 @@ if __name__ == "__main__":
         stdout, stderr = prc.communicate()
         print(stdout.decode('ascii'), stderr.decode())
         prc.wait()
+
+main()
